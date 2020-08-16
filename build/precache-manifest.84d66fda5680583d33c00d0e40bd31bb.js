91fba43c4d05723a2ba87e97ed23cabdea5c0f64
Lucas Caixeta <contato@lucascaixeta.com>
2020-08-16 20:21:21 +0100

    Sending build to be loaded in github pages

---
+++ build/precache-manifest.84d66fda5680583d33c00d0e40bd31bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85987f18198c4eb44a761b6fb122c08",
+    "url": "./index.html"
+  },
+  {
+    "revision": "03a605894db1799a3f12",
+    "url": "./static/css/2.fd3fe366.chunk.css"
+  },
+  {
+    "revision": "03a605894db1799a3f12",
+    "url": "./static/js/2.6c9096fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.6c9096fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95ea8f5db2f0483ea4bb",
+    "url": "./static/js/main.6975c0fd.chunk.js"
+  },
+  {
+    "revision": "8ac22f7331a0c9f28086",
+    "url": "./static/js/runtime-main.944ffdf1.js"
+  }
+]);
