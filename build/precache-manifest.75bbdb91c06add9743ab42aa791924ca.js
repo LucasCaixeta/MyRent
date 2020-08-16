d14946549416d0147719abb7926c215f5ccc958f
Lucas Caixeta <contato@lucascaixeta.com>
2020-08-16 20:19:13 +0100

    Initial boilerplate

---
+++ build/precache-manifest.75bbdb91c06add9743ab42aa791924ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5956d6b22c090919d1a8812e6db5194",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e881a48f5bbfb8deb0ee",
+    "url": "./static/css/2.fd3fe366.chunk.css"
+  },
+  {
+    "revision": "e881a48f5bbfb8deb0ee",
+    "url": "./static/js/2.8c07cbe6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.8c07cbe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df2b4833493a8c339145",
+    "url": "./static/js/main.d69e4895.chunk.js"
+  },
+  {
+    "revision": "8ac22f7331a0c9f28086",
+    "url": "./static/js/runtime-main.944ffdf1.js"
+  }
+]);
